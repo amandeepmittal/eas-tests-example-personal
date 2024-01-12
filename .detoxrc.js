08d6bb81233a563b582c71de50cff920e9071007
Aman Mittal <amandeepmittal@live.com>
2024-01-13 04:00:26 +0530

    apply config from doc

--- .detoxrc.js
+++ .detoxrc.js
@@ -1,91 +1,56 @@
 /** @type {Detox.DetoxConfig} */
 module.exports = {
+  logger: {
+    level: process.env.CI ? "debug" : undefined,
+  },
   testRunner: {
+    $0: "jest",
     args: {
-      $0: "jest",
       config: "e2e/jest.config.js",
+      _: ["e2e"],
     },
-    jest: {
-      setupTimeout: 120000,
-    },
-    artifacts: {
-      plugins: {
-        log: process.env.CI ? "failing" : undefined,
-        screenshot: "failing",
-      },
+  },
+  artifacts: {
+    plugins: {
+      log: process.env.CI ? "failing" : undefined,
+      screenshot: "failing",
     },
   },
   apps: {
-    "ios.debug": {
-      type: "ios.app",
-      binaryPath:
-        "ios/build/Build/Products/Debug-iphonesimulator/eastestsexample.app",
-      build:
-        "xcodebuild -workspace ios/eastestsexample.xcworkspace -scheme eastestsexample -configuration Debug -sdk iphonesimulator -derivedDataPath ios/build",
-    },
     "ios.release": {
       type: "ios.app",
+      build:
+        "xcodebuild -workspace ios/eastestsexample.xcworkspace -scheme eastestsexample -configuration Release -sdk iphonesimulator -arch x86_64 -derivedDataPath ios/build",
       binaryPath:
         "ios/build/Build/Products/Release-iphonesimulator/eastestsexample.app",
-      build:
-        "xcodebuild -workspace ios/eastestsexample.xcworkspace -scheme eastestsexample -configuration Release -sdk iphonesimulator -derivedDataPath ios/build",
-    },
-    "android.debug": {
-      type: "android.apk",
-      binaryPath: "android/app/build/outputs/apk/debug/app-debug.apk",
-      build:
-        "cd android && ./gradlew assembleDebug assembleAndroidTest -DtestBuildType=debug",
-      reversePorts: [8081],
     },
     "android.release": {
       type: "android.apk",
-      binaryPath: "android/app/build/outputs/apk/release/app-release.apk",
       build:
-        "cd android && ./gradlew assembleRelease assembleAndroidTest -DtestBuildType=release",
+        "cd android && ./gradlew :app:assembleRelease :app:assembleAndroidTest -DtestBuildType=release && cd ..",
+      binaryPath: "android/app/build/outputs/apk/release/app-release.apk",
     },
   },
   devices: {
     simulator: {
       type: "ios.simulator",
       device: {
-        type: "iPhone 15",
-      },
-    },
-    attached: {
-      type: "android.attached",
-      device: {
-        adbName: ".*",
+        type: "iPhone 14",
       },
     },
     emulator: {
       type: "android.emulator",
       device: {
-        avdName: "Pixel_3a_API_30_x86",
+        avdName: "pixel_4",
       },
     },
   },
   configurations: {
-    "ios.sim.debug": {
-      device: "simulator",
-      app: "ios.debug",
-    },
-    "ios.sim.release": {
+    "ios.release": {
       device: "simulator",
       app: "ios.release",
     },
-    "android.att.debug": {
-      device: "attached",
-      app: "android.debug",
-    },
-    "android.att.release": {
-      device: "attached",
-      app: "android.release",
-    },
-    "android.emu.debug": {
-      device: "emulator",
-      app: "android.debug",
-    },
-    "android.emu.release": {
+    "android.release": {
       device: "emulator",
       app: "android.release",
     },
